5317df8ba6f9e5f8428cf8ed94530acf3408156a
LittleBear-6w6 <57203646+LittleBear-6w6@users.noreply.github.com>
2021-05-23 18:12:16 +0900

    No remove spaces between letters

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ShellScript-CSV
 Operate CSV files by bash
+
+1. Remove space between items
+1. Add space behind items
--- script/trim-item.sh
+++ script/trim-item.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-#cat ../test/space.csv | sed -e 's/[ 　][ 　]*//g' > ../test/remove-space.csv
+cat ../test/space.csv | sed -e 's/[ 　][ 　]*//g' > ../test/remove-space.csv
 
-cat ../test/space.csv | awk 'BEGIN{ FS=",";OFS=","}{gsub("^[ 　]" , "",$1);gsub("[ 　]$" , "",$1);gsub("^[ 　]" , "",$2);gsub("[ 　]$" , "",$2);gsub("^[ 　]" , "",$3);gsub("[ 　]$" , "",$3);print $1,$2,$3}'> ../test/remove-sapceawk.csv
+#No remove space between letters
+cat ../test/space.csv | sed -e 's/,[ 　][ 　]*/, /g'  -e 's/[ 　][ 　]*,/ ,/g' | awk 'BEGIN{ FS=",";OFS=","}{sub("^[ ][ ]*" , "",$1);sub("[ ][ ]*$" , "",$1);sub("^[ ][ ]*" , "",$2);sub("[ ][ ]*$" , "",$2);sub("^[ ][ ]*" , "",$3);sub("[ ][ ]*$" , "",$3);print}'  > ../test/remove-sapceawk.csv
